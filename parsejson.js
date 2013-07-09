ee0c021e8d06eb8e51ff6d15d9c5a157d5d81e5e
刘卿 <csssnow@gmail.com>
2013-07-09 12:09:57 +0800

    Update parsejson.js

--- parsejson.js
+++ parsejson.js
@@ -14,62 +14,71 @@
 	 * @param {String} data sJSON字符串
 	 * @return {Object} JSON 对象
 	 */
-	window.JSON.parse = window.JSON.parse ? (return window.JSON.parse(data)) : function(data){
-
-		if (data === null) {
-			return data;
-		}
-
-		if (typeof data === "string") {
-		//ie左右有空格会解析失败
-			data = trim(data);
-			if (data) {
-				if (rvalidchars.test( data.replace( rvalidescape, "@" )
-				.replace( rvalidtokens, "]" )
-				.replace( rvalidbraces, "")) ) {
-					return ( new Function( "return " + data ) )();
+	if(window.JSON.parse){
+		return window.JSON.parse(data);	
+	}else{
+		window.JSON.parse = function(data){
+	
+			if (data === null) {
+				return data;
+			}
+	
+			if (typeof data === "string") {
+			//ie左右有空格会解析失败
+				data = trim(data);
+				if (data) {
+					if (rvalidchars.test( data.replace( rvalidescape, "@" )
+					.replace( rvalidtokens, "]" )
+					.replace( rvalidbraces, "")) ) {
+						return ( new Function( "return " + data ) )();
+					}
 				}
 			}
-		}
-		throw new Error( "Invalid JSON: " + data );
+			throw new Error( "Invalid JSON: " + data );
+		};
 	};
 	/**
 	 * [ description]
 	 * @param  {Object} data JSON对象
 	 * @return {String} JSON字符串
 	 */
-	window.JSON.stringify = window.JSON.stringify ? (return window.JSON.stringify(data)) : function(data){
-		var tArr = [];
-		//数组使用递归
-		if(Apply(data) === "[object Array]"){
-			for (var i = 0; i < data.length; i++) {
-				tArr.push(JSON.stringify(data[i]));
-			};
-			return "[" + tArr.join(',') + "]";
-		}
-		//日期直接返回时间戳
-		if (Apply(data) === "[object Date]") {
-			return "new Date("+ data.getTime() +")";
-		};
-		//正则或函数直接使用toString
-		if (Apply(data) === "[object RegExp]" || Apply(data) === "[object Function]") {
-			return data.toString();
-		};
-		//对象使用for in递归
-		if (Apply(data) === "[object Object]") {
-			for(var i in data){
-				if (typeof data[i] === "string") {
-					data[i] = "'" + data[i] + "'";
-				}else{
-					if(typeof data[i] === "object"){
-						data[i] = JSON.stringify(data[i]);
-					}else{
-						data[i] = "'" + data[i] + "'";
-					}
-				}
-				tArr.push(i + ": " + data[i]);
+	if(window.JSON.stringify){
+		return window.JSON.stringify(data);	
+	}else{
+		window.JSON.stringify = function(data){
+			var tArr = [];
+			//数组使用递归
+			if(Apply(data) === "[object Array]"){
+				for (var i = 0; i < data.length; i++) {
+					tArr.push(JSON.stringify(data[i]));
+				};
+				return "[" + tArr.join(',') + "]";
 			}
-			return "{" + tArr.join(",") + "}";			
-		};
+			//日期直接返回时间戳
+			if (Apply(data) === "[object Date]") {
+				return "new Date("+ data.getTime() +")";
+			};
+			//正则或函数直接使用toString
+			if (Apply(data) === "[object RegExp]" || Apply(data) === "[object Function]") {
+				return data.toString();
+			};
+			//对象使用for in递归
+			if (Apply(data) === "[object Object]") {
+				for(var i in data){
+					if (typeof data[i] === "string") {
+						data[i] = "'" + data[i] + "'";
+					}else{
+						if(typeof data[i] === "object"){
+							data[i] = JSON.stringify(data[i]);
+						}else{
+							data[i] = "'" + data[i] + "'";
+						}
+					}
+					tArr.push(i + ": " + data[i]);
+				}
+				return "{" + tArr.join(",") + "}";			
+			};
+		}	
 	}
+	
 }(this));
