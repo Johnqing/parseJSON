2e60088235cab2628c1566dd68616444fffd3a70
刘卿 <csssnow@gmail.com>
2013-07-09 12:03:25 +0800

    Update parsejson.js

--- parsejson.js
+++ parsejson.js
@@ -73,24 +73,3 @@
 		};
 	}
 }(this));
-
-
-
-var O2String = function (O) {
-       //return JSON.stringify(jsonobj);
-
-       var S = [];
-       var J = "";
-       else if (Object.prototype.toString.apply(O) === '[object RegExp]' || Object.prototype.toString.apply(O) === '[object Function]') {
-           J = O.toString();
-       }
-       else if (Object.prototype.toString.apply(O) === '[object Object]') {
-           for (var i in O) {
-               O[i] = typeof (O[i]) == 'string' ? '"' + O[i] + '"' : (typeof (O[i]) === 'object' ? O2String(O[i]) : O[i]);
-               S.push(i + ':' + O[i]);
-           }
-           J = '{' + S.join(',') + '}';
-       }
-
-       return J;
-};
